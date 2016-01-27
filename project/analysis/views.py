c90cbc9ea007b19c5337cb16dcbc08a464eae704
McLaren <mclaren@McLarens-MacBook-Pro.local>
2016-01-27 17:31:01 -0500

    analyse and securities froms

--- project/analysis/views.py
+++ project/analysis/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .analysis_models import Client, Risk, Investment_policy
+from .analysis_forms import ClientForm, RiskForm, Investment_policyForm 
 
-# Create your views here.
+class ClientCreateView(View):
+    template_name = 'analysis/index.html'
+    model = Client
+    form_class = ClientForm
+
+    def get(self, request):
+        context = dict(form=self.form_class(),submit_text='Create')
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            client = form.save()
+            return redirect('analysis:index')
+        else:
+            context = dict(form=form,submit_text='Create')
+            return render(request, self.template_name, context)
