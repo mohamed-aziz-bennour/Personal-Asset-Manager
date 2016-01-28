9f7f1c91564132ca2fbdceb9ce594cf9d767b4fb
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-01-28 13:30:09 -0500

    create a portfolio

--- project/portfolio/views.py
+++ project/portfolio/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from portfolio.models import Portfolio, Asset
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from portfolio.forms import PortfolioForm
+
 
 # Create your views here.
+
+class CreatePortfolio(LoginRequiredMixin,View):
+    template_name = "portfolio/portfolio_form.html"
+    form_class = PortfolioForm
+    success_url = "portfolio:create"
+    context = None
+
+    def get(self,request):
+        form = self.form_class()
+        self.context = self.get_context(form)
+        return self.render_to_response(request)
+
+    def post(self,request):
+        form = self.form_class(data=request.POST)
+        data=request.POST
+        if form.is_valid(): 
+            portfolio = Portfolio.objects.create(
+                user = request.user,
+                portfolio_name=data.get('portfolio_name')
+                )
+            
+        
+            return redirect(self.success_url)
+        else:
+            self.context = self.get_context(form)
+            return self.render_to_response(request)
+
+
+        
+    def get_context(self, form):
+        return {
+            "form": form,
+            "action": "portfolio:create",
+            "method": "POST",
+            "submit_text": "Add new Portfolio"
+        }
+
+    def render_to_response(self, request):
+        return render(request, self.template_name, self.context)
+
+
