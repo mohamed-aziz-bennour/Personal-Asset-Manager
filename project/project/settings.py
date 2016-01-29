1b07eaf48b792af1d183a60d56c507cf6fb807a8
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-01-29 11:39:47 -0500

    fixing the settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_forms_bootstrap',
     'crispy_forms',
     'users',
     'securities',
