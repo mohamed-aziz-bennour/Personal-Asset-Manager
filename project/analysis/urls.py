55d8be7d5538bf5a2a152524e97075f4944ba496
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-01-30 20:59:52 -0500

    calculate score

--- project/analysis/urls.py
+++ project/analysis/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 	url(r'^analysis$', views.ClientCreateView.as_view(), name="analysis"),
+	url(r'^risk$', views.RiskView.as_view(), name="risk"),
+
 	]
 
 
--- project/analysis/views.py
+++ project/analysis/views.py
@@ -2,35 +2,67 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from .analysis_models import Client, Risk, Investment_policy
 from .analysis_forms import ClientForm, RiskForm, Investment_policyForm 
+from django.http import JsonResponse
 
 class ClientCreateView(View):
-	template_name = 'analysis/index.html'
-	model = Client
-	form_class = ClientForm
-	context = None
+    template_name = 'analysis/index.html'
+    model = Client
+    form_class = ClientForm
+    context = None
 
-	def get(self, request):
-		print('I am in the get')
-		form = self.form_class()
-		print(form)
-		# context = dict(form=self.form_class(),submit_text='Create')
-		self.context = self.get_context(form)
-		print(self.context)
-		return render(request, self.template_name, self.context)
+    def get(self, request):
+        form = self.form_class()
+        self.context = self.get_context(form)
+        return render(request, self.template_name, self.context)
 
-	def post(self, request):
-		form = self.form_class(data=request.POST)
-		if form.is_valid():
-			client = form.save()
-			return redirect('analysis:index')
-		else:
-			context = dict(form=form,submit_text='Create')
-			return render(request, self.template_name, context)
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            client = form.save()
+            return redirect('analysis:index')
+        else:
+            context = dict(form=form,submit_text='Create')
+            return render(request, self.template_name, context)
 
-	def get_context(self, form):
-		return {
-			"form": form,
-			"action": "analysis:analysis",
-			"method": "POST",
-			"submit_text": "Create Client"
-		}
+    def get_context(self, form):
+        return {
+            "form": form,
+            "action": "analysis:analysis",
+            "method": "POST",
+            "submit_text": "Create Client"
+        }
+
+
+class RiskView(View):
+    template_name = 'analysis/index.html'
+    model = Risk
+    form_class = RiskForm
+    context = None
+
+    def get(self, request):
+        form = self.form_class()
+        self.context = self.get_context(form)
+        return render(request, self.template_name, self.context)
+
+    def post(self, request):
+        form = self.form_class(data=request.POST)
+        data=request.POST
+        if form.is_valid():
+            score = int(data.get('cash_reserves')[0]) + int(data.get('time_horizont')[0])
+            score += int(data.get('market_loss')[0]) + int(data.get('investment_experience')[0]) 
+            score += int(data.get('investment_return')[0])
+            print(score)
+
+            # risk = form.save()
+            return JsonResponse({'score': score})
+        else:
+            context = dict(form=form,submit_text='Create')
+            return render(request, self.template_name, context)
+
+    def get_context(self, form):
+        return {
+            "form": form,
+            "action": "analysis:risk",
+            "method": "POST",
+            "submit_text": "Submit Risk Form"
+        }
