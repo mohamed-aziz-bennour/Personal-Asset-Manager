1a3608280decaf072c1075b4c651b16c35175667
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-02-02 13:20:02 -0500

    return json list of stock and etf

--- project/securities/securities_models.py
+++ project/securities/securities_models.py
@@ -5,33 +5,55 @@ from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Stock(models.Model):
-	symbol = models.CharField(max_length = 15)
-	company_name = models.CharField(max_length = 255)
-	sector = models.CharField(max_length = 30)
-	industry = models.CharField(max_length = 30)
-	market_capitalization = models.DecimalField(max_digits = 10, 
-		decimal_places = 2
-		)
-	# assets = GenericRelation(Asset)
+    symbol = models.CharField(max_length = 15)
+    company_name = models.CharField(max_length = 255)
+    sector = models.CharField(max_length = 30)
+    industry = models.CharField(max_length = 30)
+    market_capitalization = models.DecimalField(max_digits = 10, 
+        decimal_places = 2
+        )
+    def to_json(self):
+        return dict(
+            id = self.id,
+            symbol = self.symbol,
+            company_name = self.company_name,
+            sector = self.sector,
+            industry = self.industry,
+            market_capitalization = self.market_capitalization
+
+        )
+    # assets = GenericRelation(Asset)
 
 
 class Bond(models.Model):
-	symbol = models.CharField(max_length = 15)
-	company_name = models.CharField(max_length = 255)
-	coupon = models.DecimalField(max_digits = 2, decimal_places = 2)
-	maturity_date = models.DateTimeField(auto_now_add = False)
-	bond_type = models.CharField(max_length = 15)
-	# assets = GenericRelation(Asset)
+    symbol = models.CharField(max_length = 15)
+    company_name = models.CharField(max_length = 255)
+    coupon = models.DecimalField(max_digits = 2, decimal_places = 2)
+    maturity_date = models.DateTimeField(auto_now_add = False)
+    bond_type = models.CharField(max_length = 15)
+    # assets = GenericRelation(Asset)
 
 
 class ExchangeTradedFund(models.Model):
-	symbol = models.CharField(max_length = 15)
-	name = models.CharField(max_length = 255)
-	category = models.CharField(max_length = 30)
-	fund_family = models.CharField(max_length = 30)
-	beta = models.FloatField(null=True,blank=True)
-	alpha = models.FloatField(null=True,blank=True)
-	r_squared = models.FloatField(null=True,blank=True)
-	sharpe_ratio = models.FloatField(null=True,blank=True)
-	# assets = GenericRelation(Asset)
+    symbol = models.CharField(max_length = 15)
+    name = models.CharField(max_length = 255)
+    category = models.CharField(max_length = 30)
+    fund_family = models.CharField(max_length = 30)
+    beta = models.FloatField(null=True,blank=True)
+    alpha = models.FloatField(null=True,blank=True)
+    r_squared = models.FloatField(null=True,blank=True)
+    sharpe_ratio = models.FloatField(null=True,blank=True)
+    # assets = GenericRelation(Asset)
+    def to_json(self):
+        return dict(
+            id = self.id,
+            symbol = self.symbol,
+            name = self.name,
+            category = self.category,
+            fund_family= self.fund_family,
+            beta = self.beta,
+            alpha = self.alpha,
+            r_squared = self.r_squared,
+            sharpe_ratio = self.sharpe_ratio
+        )
 
