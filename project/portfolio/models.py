56df1a71b0438008cc138d98d0dcf682022fa486
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-02-05 23:24:08 -0500

    weigth and total value

--- project/portfolio/models.py
+++ project/portfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from decimal import *
   
 
 # Create your models here.
@@ -27,17 +28,19 @@ class Asset(models.Model):
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     content_object = GenericForeignKey('content_type', 'object_id') 
+
     
 
 
-    def to_json(self):
+    def to_json(self,value,portfolio_value):
         return dict(
             id = self.id,
             quantity = self.quantity ,
             cost_basis = self.cost_basis ,
             content_object = self.content_object.to_json(),
             content_type = str(self.content_type),
-            object_id = str(self.object_id)
-            
+            object_id = str(self.object_id), 
+            value = value,
+            weight = '%' + str( round(value * 100 / portfolio_value,2))
 
         )
